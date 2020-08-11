4b780f5410ff0e6b22ec78e63b2459be079c7c3e
wonderfulsunshine <1137674424@qq.com>
2020-08-11 15:33:50 +0800

    second

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 全局样式
+import "./assets/css/global.css"
+import ElementUi from "element-ui"
+// element-Ui样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 导入字体图标
+import "./assets/fonts/iconfont.css"
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUi)
 
 // http://119.23.53.78:8888/api/private/v1
 
